b14c1e184990b72cf2eada83eae86182005b663d
FloweryWisdom <srgerardoflorencio@gmail.com>
2023-08-25 22:01:32 -0600

    created koders & practice models and created .env file with db login

--- API-Koders-Clean/index.js
+++ API-Koders-Clean/index.js
@@ -1,5 +1,20 @@
+require('dotenv').config()
+const mongoose = require("mongoose") 
 const server = require("./src/server")
 
-server.listen(8080, () => {
-  console.log("Sever listening on port 8080")
-})
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST
+const DB_NAME = process.env.DB_NAME
+
+mongoose
+  .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`)
+  .then(() => {
+    console.log("Connected to DB")
+    server.listen(8080, () => {
+      console.log("Sever listening on port 8080")
+    })
+  })
+  .catch((error) => {
+    console.log("DB error", error)
+  })
